cee334cdf04ab27f1cfc9259c366cac803e100c6
Rupa Dey <deyrupa525@gmail.com>
2022-11-09 10:36:44 +0600

    stateless-Widget

--- main.dart
+++ main.dart
@@ -1,14 +1,27 @@
-
 import 'package:flutter/material.dart';
-  void main()
-  {
-    
-    runApp(
-      new Center(
-        child:  new Text('This is my first flutter application!!!',textDirection: TextDirection.ltr,textAlign: TextAlign.center,),
-      )
 
-   );
+void main() {
+  runApp(new application());
+}
+
+//stateless widget
+class application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'stateless widget',
+        home: new Scaffold(
+          body: Container(
+            color: Colors.cyan,
+            child: new Container(
+              color: Colors.red,
+              margin: const EdgeInsets.all(50.0),
+              child: new Container(
+                color: Colors.amber,
+                margin: const EdgeInsets.all(40.0),
+              ),
+            ),
+          ),
+        ));
   }
-
- 
+}
