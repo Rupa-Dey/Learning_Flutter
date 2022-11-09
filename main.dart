f7ef155327343159aacb028fd9f29f77a993191b
Rupa Dey <deyrupa525@gmail.com>
2022-11-09 10:21:42 +0600

    first flutter application code

---
+++ main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+  void main()
+  {
+    
+    runApp(
+      new Center(
+        child:  new Text('This is my first flutter application!!!',textDirection: TextDirection.ltr,textAlign: TextAlign.center,),
+      )
+
+   );
+  }
+
+ 
