b64fd2db162e3fdd3872e3674efd4d02be0c7c75
Rupa Dey <deyrupa525@gmail.com>
2022-11-09 22:59:56 +0600

    the code is stack card implementation..

--- main.dart
+++ main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(home : new application()));
+  runApp(new MaterialApp(home: new application()));
 }
 
 class application extends StatefulWidget {
-
   @override
   State<application> createState() => _applicationState();
 }
@@ -14,18 +13,41 @@ class _applicationState extends State<application> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
       body: new Stack(
-          mainAxisAlignment: MainAxisAlignment.center,
-        //alignment: Alignment.center,
-     
+        // mainAxisAlignment: MainAxisAlignment.center,
+        alignment: Alignment.center,
+
         children: <Widget>[
-          new Card(color: Colors.amber, child: new Padding(padding: const EdgeInsets.all(200.0),),),
-          new Card(color: Colors.red, child: new Padding(padding: const EdgeInsets.all(130.0),),),
-          new Card(color: Colors.green, child: new Padding(padding: const EdgeInsets.all(80.0),),),
-          new Card(color: Colors.cyan, child: new Padding(padding: const EdgeInsets.all(45.0),),),
-          new Card(color: Colors.purple, child: new Padding(padding: const EdgeInsets.all(15.0),),),
-        
+          new Card(
+            color: Colors.amber,
+            child: new Padding(
+              padding: const EdgeInsets.all(200.0),
+            ),
+          ),
+          new Card(
+            color: Colors.red,
+            child: new Padding(
+              padding: const EdgeInsets.all(130.0),
+            ),
+          ),
+          new Card(
+            color: Colors.green,
+            child: new Padding(
+              padding: const EdgeInsets.all(80.0),
+            ),
+          ),
+          new Card(
+            color: Colors.cyan,
+            child: new Padding(
+              padding: const EdgeInsets.all(45.0),
+            ),
+          ),
+          new Card(
+            color: Colors.purple,
+            child: new Padding(
+              padding: const EdgeInsets.all(15.0),
+            ),
+          ),
         ],
       ),
     );
