b6659e1e39813751757137583230ea64e6f6b8c3
Rupa Dey <deyrupa525@gmail.com>
2022-11-09 12:14:16 +0600

    In this part widget will work dynamically

--- main.dart
+++ main.dart
@@ -4,24 +4,66 @@ void main() {
   runApp(new application());
 }
 
-//stateless widget
-class application extends StatelessWidget {
+//..............
+// // statefull widget
+class application extends StatefulWidget {
+  @override
+  State<application> createState() => _applicationState();
+}
+
+class _applicationState extends State<application> {
+  String titleText = '';
+  @override
+  void initState() {
+    titleText = 'Click here--->';
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void method1() {
+    setState(() {
+      titleText = 'the text has been changed.';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'stateless widget',
-        home: new Scaffold(
-          body: Container(
-            color: Colors.cyan,
-            child: new Container(
-              color: Colors.red,
-              margin: const EdgeInsets.all(50.0),
-              child: new Container(
-                color: Colors.amber,
-                margin: const EdgeInsets.all(40.0),
-              ),
-            ),
-          ),
-        ));
+      title: 'Statefull widget',
+      home: new Scaffold(
+          body: new Center(
+              //body is the primary property of scaffold
+        child: new RaisedButton(onPressed:() {   method1();   },
+        child: new Text(titleText),
+      ),),),
+    );
   }
 }
+
+
+// .......................
+//   //..................
+// 
+
+
+// //stateless widget
+// class application extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//         title: 'stateless widget',
+//         home: new Scaffold(
+//           body: Container(
+//             color: Colors.cyan,
+//             child: new Container(
+//               color: Colors.red,
+//               margin: const EdgeInsets.all(50.0),
+//               child: new Container(
+//                 color: Colors.amber,
+//                 margin: const EdgeInsets.all(40.0),
+//               ),
+//             ),
+//           ),
+//         ));
+//   }
+// }
