1c019b8e0d6e3c488c841838bb30ee05d852b805
Rupa Dey <deyrupa525@gmail.com>
2022-11-10 14:14:50 +0600

    here is created text field and Button field.

--- main.dart
+++ main.dart
@@ -1,21 +1,62 @@
 import 'package:flutter/material.dart';
-import 'custom.dart';
+//import 'custom.dart';
 
 void main() {
   runApp(new application());
 }
 
-class application extends StatelessWidget {
-  //
-  //const application({Key? key}) : super(key: key);
+class application extends StatefulWidget {
+  const application({Key? key}) : super(key: key);
 
   @override
+  State<application> createState() => _applicationState();
+}
+
+class _applicationState extends State<application> {
+  @override
+  String ptext = ' ';
+  String btext = ' ';
+  void method1(String value) {
+    setState(() {
+      btext = value;
+    });
+  }
+
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'custom Widget',
+      title: 'myApplication',
       home: new Scaffold(
-        body: new customwidgets(),
-      ),
+          appBar: new AppBar(
+            backgroundColor: Colors.red,
+            title: new Text('myApplication'),
+          ),
+          body: new Column(
+            children: <Widget>[
+              new TextField(
+                onChanged: (String txt) {
+                  setState(
+                    () {
+                      ptext = txt;
+                    },
+                  );
+                },
+                decoration: new InputDecoration(
+                    hintText: 'type here', labelText: 'Name'),
+              ),
+              new Text(ptext),
+              new RaisedButton(
+                onPressed: () {
+                  method1('You have pressed');
+                },
+                child: new Text('click'),
+              ),
+              new FlatButton(onPressed: () {
+                method1('fButton is pressed');
+              }, child: new Text('Fbutton'),),
+
+              new Text(btext),
+            ],
+          )),
     );
   }
 }
